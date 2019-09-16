a749fb1f1a435b3b51e91f580073f435f05a5cc1
Ozmar Ugarte <ozmar_11@hotmail.com>
2019-09-16 16:20:35 -0500

    Remove node modules

--- src/base.js
+++ src/base.js
@@ -2,7 +2,7 @@ import Rebase from "re-base";
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyD0JrQipdkRqfUlhWpVrwGt0D4kUGMbYOQ",
+    apiKey: "",
     authDomain: "catch-of-the-day-8b01e.firebaseapp.com",
     databaseURL: "https://catch-of-the-day-8b01e.firebaseio.com"
 });
